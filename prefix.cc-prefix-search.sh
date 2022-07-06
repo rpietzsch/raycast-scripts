56fd3400de26c487c05ff4c58ee1df6dbd56a1e8
Rene Pietzsch <rene.pietzsch@eccenca.com>
2022-07-06 10:12:15 +0200

    first commit

---
+++ prefix.cc-prefix-search.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title prefix.cc search
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🔗
+# @raycast.packageName RDF Tools
+# @raycast.argument1 { "type": "text", "placeholder": "prefix" }
+# @raycast.argument2 { "type": "text", "placeholder": "return: ns, ttl, sparql*", "optional": true }
+
+# Documentation:
+# @raycast.author rpietzsch
+# @raycast.authorURL https://github.com/rpietzsch/
+
+NS=`curl -s http://prefix.cc/${1}.file.ini | cut -d '=' -f 2`
+
+if [ "${2}" = "ns" ] ; then
+  echo $NS
+elif  [ "${2}" = "ttl" ] ; then
+  echo "@prefix ${1}: <${NS}> ."
+else
+  echo "PREFIX ${1}: <${NS}>"
+fi
+
