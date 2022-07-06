1b9ae9827a1ca6068f7773d0fa773bf71967c21b
Rene Pietzsch <rene.pietzsch@eccenca.com>
2022-07-06 16:15:42 +0200

    add vowl action

--- prefix.cc-prefix-search.sh
+++ prefix.cc-prefix-search.sh
@@ -9,7 +9,7 @@
 # @raycast.icon 🔗
 # @raycast.packageName RDF Tools
 # @raycast.argument1 { "type": "text", "placeholder": "prefix" }
-# @raycast.argument2 { "type": "text", "placeholder": "return: sparql*, ns, ttl, open", "optional": true }
+# @raycast.argument2 { "type": "text", "placeholder": "return: sparql*, ns, ttl, open, vowl", "optional": true }
 
 # Documentation:
 # @raycast.author rpietzsch
@@ -17,12 +17,17 @@
 
 NS=$(curl -s http://prefix.cc/${1}.file.ini | cut -d '=' -f 2)
 
+# cf. https://stackoverflow.com/questions/296536/how-to-urlencode-data-for-curl-command
+NS_=$(perl -MURI::Escape -e "print uri_escape(\"${NS}\");")
+
 if [ "${2}" = "ns" ] ; then
   echo "${NS}"
 elif  [ "${2}" = "ttl" ] ; then
   echo "@prefix ${1}: <${NS}> ."
 elif  [ "${2}" = "open" ] ; then
-  open "${NS}"
+  open -u "${NS}"
+elif  [ "${2}" = "vowl" ] ; then
+  open -u "https://service.tib.eu/webvowl/#iri=${NS_}"
 else
   echo "PREFIX ${1}: <${NS}>"
 fi
